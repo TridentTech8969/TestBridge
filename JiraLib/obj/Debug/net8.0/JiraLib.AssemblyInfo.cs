7bf3fc23caa7892cb491f450cdf6fd6922c42dbe
TridentTech8969 <eternalvision2025@gmail.com>
2026-04-18 01:09:55 +0530

    Clean repo

--- JiraLib/obj/Debug/net8.0/JiraLib.AssemblyInfo.cs
+++ JiraLib/obj/Debug/net8.0/JiraLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JiraLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f986209e509c68d6883719bec8e9e18155d016")]
 [assembly: System.Reflection.AssemblyProductAttribute("JiraLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JiraLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
